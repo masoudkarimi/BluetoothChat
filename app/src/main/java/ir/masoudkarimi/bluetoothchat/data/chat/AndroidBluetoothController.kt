78f4e04c9a33342f52411f8b6abdc17f4e61ec77
masoud <karimi.masoud5494@gmail.com>
2023-12-16 21:57:48 +0330

    create BluetoothController

---
+++ app/src/main/java/ir/masoudkarimi/bluetoothchat/data/chat/AndroidBluetoothController.kt
@@ -0,0 +1,92 @@
+package ir.masoudkarimi.bluetoothchat.data.chat
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import ir.masoudkarimi.bluetoothchat.domain.chat.BluetoothController
+import ir.masoudkarimi.bluetoothchat.domain.chat.BluetoothDeviceDomain
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+@SuppressLint("MissingPermission")
+class AndroidBluetoothController(
+    private val context: Context
+) : BluetoothController {
+
+    // This is null when the device doesn't support Bluetooth
+    private val bluetoothManager: BluetoothManager? by lazy {
+        context.getSystemService(BluetoothManager::class.java)
+    }
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val _scannedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
+    override val scannedDevices = _scannedDevices.asStateFlow()
+
+    private val _pairedDevices = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
+    override val pairedDevices = _pairedDevices.asStateFlow()
+
+    private val foundDeviceReceiver = FoundDeviceReceiver { device ->
+        _scannedDevices.update { devices ->
+            val newDevice = device.toBluetoothDeviceDomain()
+
+            if (newDevice in devices) {
+                devices
+            } else {
+                devices + newDevice
+            }
+        }
+    }
+
+    init {
+        updatePairedDevices()
+    }
+
+    override fun startDiscovery() {
+        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            return
+        }
+
+        context.registerReceiver(
+            foundDeviceReceiver,
+            IntentFilter(BluetoothDevice.ACTION_FOUND)
+        )
+        updatePairedDevices()
+        bluetoothAdapter?.startDiscovery()
+    }
+
+    override fun stopDiscovery() {
+        if (!hasPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            return
+        }
+
+        bluetoothAdapter?.cancelDiscovery()
+    }
+
+    override fun release() {
+        context.unregisterReceiver(foundDeviceReceiver)
+    }
+
+    private fun updatePairedDevices() {
+        if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+            return
+        }
+
+        bluetoothAdapter
+            ?.bondedDevices
+            ?.map(BluetoothDevice::toBluetoothDeviceDomain)
+            ?.also { devices -> _pairedDevices.update { devices } }
+    }
+
+    private fun hasPermission(permission: String): Boolean {
+        return context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+    }
+}
---
+++ app/src/main/java/ir/masoudkarimi/bluetoothchat/domain/chat/BluetoothDevice.kt
@@ -0,0 +1,9 @@
+package ir.masoudkarimi.bluetoothchat.domain.chat
+
+typealias BluetoothDeviceDomain = BluetoothDevice
+
+data class BluetoothDevice(
+    val name: String?,
+    val address: String,
+
+)
