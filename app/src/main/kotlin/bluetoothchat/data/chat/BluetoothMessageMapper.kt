13adce720d298bb6fac582893c6beb59e8be2e1b
masoud <karimi.masoud5494@gmail.com>
2023-12-21 17:48:16 +0330

    create message class for chatting and its mapper functions

---
+++ app/src/main/kotlin/bluetoothchat/data/chat/BluetoothMessageMapper.kt
@@ -0,0 +1,18 @@
+package bluetoothchat.data.chat
+
+import bluetoothchat.domain.chat.BluetoothMessage
+
+
+fun String.toBluetoothMessage(isFromLocalUser: Boolean): BluetoothMessage {
+    val name = substringBeforeLast("#")
+    val message = substringAfter("#")
+    return BluetoothMessage(
+        senderName = name,
+        message = message,
+        isFromLocalUser = isFromLocalUser
+    )
+}
+
+fun BluetoothMessage.toByteArray(): ByteArray {
+    return "$senderName#$message".encodeToByteArray()
+}
